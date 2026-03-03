7de1e7af439ffee68c23e3b190eddda387328867
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-03-02 23:40:17 -0300

    Fix forEachIndexed macro not actually using the passed index variable name

--- src/utils.h
+++ src/utils.h
@@ -2,7 +2,7 @@
 
 #define forEach(item, array, count) for (typeof(count) _i = 0; _i < (count) && ((item) = &(array)[_i], 1); _i++)
 
-#define forEachIndexed(item, index, array, count) for (typeof(count) idx = 0; idx < (count) && ((item) = &(array)[idx], 1); idx++)
+#define forEachIndexed(item, index, array, count) for (typeof(count) index = 0; index < (count) && ((item) = &(array)[index], 1); index++)
 
 #define repeat(n, it) for (typeof(n) it = 0; it < (n); it++)
 
